f6caa4dcac1439f821b67d696087ad2ed1d85cb7
James Rogers <jarogers095@gmail.com>
2020-04-01 21:43:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
     resp = Rack::Response.new()
     req = Rack::Request.new(env)
     if req.path.include?("/items/")
+      requested_item = Item.new
       item_name = req.path.split("/items/").last
       @@items.each do |i|
         if i.name == item_name
