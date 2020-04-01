b4de63bdf4ab9b35116be98667a40dc5b622e0bd
James Rogers <jarogers095@gmail.com>
2020-04-01 21:35:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,10 @@ class Application
   def call(env)
     resp = Rack::Response.new()
     req = Rack::Request.new(env)
-    if req.path.contains?("/items/")
+    if req.path.include?("/items/")
       item = req.path.split("/items/").last
-      
+      if @@items.include?(item)
+        resp.write()
       
   end
 end
