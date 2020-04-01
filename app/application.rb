f9097787c7480aaab866034763e67055c676d70d
James Rogers <jarogers095@gmail.com>
2020-04-01 21:46:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,25 @@ class Application
   def call(env)
     resp = Rack::Response.new()
     req = Rack::Request.new(env)
+    
     if req.path.include?("/items/")
-      requested_item = Item.new
+      found_item = nil
       item_name = req.path.split("/items/").last
       @@items.each do |i|
         if i.name == item_name
-          resp.write(i.price)
-          
+          found_item = i
+        end
+      end
       
+      if found_item
+        resp.write(found_item.price)
+      else
+        resp.status = 400
+        resp.write("Item not found")
+      end
+    else
+      resp.status = 404
+      resp.write("File not found")
+    end
   end
 end
