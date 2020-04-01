3db9048d6b0eafc64347eb2a978b7b72c4a73746
James Rogers <jarogers095@gmail.com>
2020-04-01 21:32:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,12 @@
 class Application
+  @@items = []
+  
   def call(env)
     resp = Rack::Response.new()
     req = Rack::Request.new(env)
-    
+    if req.path.contains?("/items/")
+      item = req.path.split("/items/").last
+      
+      
   end
 end
